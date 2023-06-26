6abaa624aae34daff1373ff2e105029bfaf56b90
Gabriela T <gtrujillo15@gmail.com>
2023-06-26 16:28:01 -0700

    feat(js):add js scripts and fade-in animation on scroll

--- themes/portfolio/functions.php
+++ themes/portfolio/functions.php
@@ -142,6 +142,7 @@ function portfolio_scripts() {
 	wp_style_add_data( 'portfolio-style', 'rtl', 'replace' );
 
 	wp_enqueue_script( 'portfolio-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+	wp_enqueue_script( 'portfolio-scripts', get_template_directory_uri() . '/js/scripts.js', array('jquery'), null, true);
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
